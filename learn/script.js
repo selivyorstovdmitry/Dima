93eab1b3baedb933927e58660fbaabb9c326c587
Dima <sedrik205@mail.ru>
2021-06-28 20:07:36 +0300

    3 test

--- learn/script.js
+++ learn/script.js
@@ -1,11 +1,21 @@
-'use strict';
-
-const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", 10);
-
-alert( "вы посмтрели " + numberOfFilms);
 
 
-let personslMovieDB = {
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+    
+    while (numberOfFilms == '' || numberOfFilms == null ||  isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+    }
+
+}
+start();
+
+
+
+
+const personslMovieDB = {
 count: numberOfFilms,
 movies:  {},
 actors: {},
@@ -15,65 +25,44 @@ privat: false,
 };
 
 
-/* for(let i = 0; i < 2 ;i++  ){
-    const a = prompt("Один из последних фильмов?", ""),
-          b = prompt("На сколько оцените его?", 10);
-          personslMovieDB.movies[a]=b;
-    if (a != null && b != null && a != "" && b != "" && a.length <  50){
-        console.log(personslMovieDB);
-    }else{
-        i--;
-    }
-
-} */
-
-let i =0 ;
-while (i < 2){
-    const a = prompt("Один из последних фильмов?", ""),
-          b = prompt("На сколько оцените его?", 10);
-          i++;
-          personslMovieDB.movies[a]=b;
-    if (a != null && b != null && a != "" && b != "" && a.length <  50){
-        console.log(personslMovieDB);
-    }else{
-        i--;
+function rememberMyFilms(){
+    for(let i = 0; i < 2 ;i++  ){
+        const a = prompt("Один из последних фильмов?", ""),
+              b = prompt("На сколько оцените его?", 10);
+              personslMovieDB.movies[a]=b;
+        if (a != null && b != null && a != "" && b != "" && a.length <  50){
+            console.log(personslMovieDB);
+        }else{
+            i--;
+        }
+    
     }
 }
+/* rememberMyFilms(); */
 
-if (personslMovieDB.count < 10 ){
-    alert("Просмотрено довольно мало фильмов");
-}else if (personslMovieDB.count >= 10 && personslMovieDB.count < 30){
-    alert("Вы классический зритель");
-}else if(personslMovieDB.count>= 30){
-    alert("Вы киноман");
-}else{
-    alert("Произошла ошибка");
+function detectPersonalMovie (){
+    if (personslMovieDB.count < 10 ){
+        alert("Просмотрено довольно мало фильмов");
+    }else if (personslMovieDB.count >= 10 && personslMovieDB.count < 30){
+        alert("Вы классический зритель");
+    }else if(personslMovieDB.count>= 30){
+        alert("Вы киноман");
+    }else{
+        alert("Произошла ошибка");
+    }
 }
-
-
-
-
-
-
-
-
-
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-
-
-
-
-
+/* detectPersonalMovie ();
+ */
+function showMyDB (){
+    if(personslMovieDB.privat == false){
+        console.log(personslMovieDB)
+    }
+}
+showMyDB();
+function writeYourGeneres(){
+ for(let i = 1; i <=3 ; i++){
+     const ganre = prompt(`Ваш любимый жанр под номером ${i}`);
+     personslMovieDB.genres[i -1] = ganre;
+ }
+}
+writeYourGeneres();
