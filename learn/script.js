686460b19995b34c17f626ddec8bd76d03aa730a
Dima <sedrik205@mail.ru>
2021-06-28 15:18:44 +0300

    second test

--- learn/script.js
+++ learn/script.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", 10);
 
@@ -12,15 +13,66 @@ genres: [],
 privat: false,
 
 };
-const a = prompt("Один из последних фильмов?", ""),
-       b = prompt("На сколько оцените его?", 10),
-       c =prompt("Один из последних фильмов?", ""),
-       v = prompt("На сколько оцените его?", 10);
 
-personslMovieDB.movies[a]=b;
-personslMovieDB.movies[c]=v;
 
-console.log(personslMovieDB);
+/* for(let i = 0; i < 2 ;i++  ){
+    const a = prompt("Один из последних фильмов?", ""),
+          b = prompt("На сколько оцените его?", 10);
+          personslMovieDB.movies[a]=b;
+    if (a != null && b != null && a != "" && b != "" && a.length <  50){
+        console.log(personslMovieDB);
+    }else{
+        i--;
+    }
+
+} */
+
+let i =0 ;
+while (i < 2){
+    const a = prompt("Один из последних фильмов?", ""),
+          b = prompt("На сколько оцените его?", 10);
+          i++;
+          personslMovieDB.movies[a]=b;
+    if (a != null && b != null && a != "" && b != "" && a.length <  50){
+        console.log(personslMovieDB);
+    }else{
+        i--;
+    }
+}
+
+if (personslMovieDB.count < 10 ){
+    alert("Просмотрено довольно мало фильмов");
+}else if (personslMovieDB.count >= 10 && personslMovieDB.count < 30){
+    alert("Вы классический зритель");
+}else if(personslMovieDB.count>= 30){
+    alert("Вы киноман");
+}else{
+    alert("Произошла ошибка");
+}
+
+
+
+
+
+
+
+
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+
 
 
 
